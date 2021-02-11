0c2fd3066e775b5a8ef7360055d83416ac3022d5
Paulo Truta <pinheirotruta5@gmail.com>
2021-02-11 19:56:35 +0100

    Fixed Dockerfile and compose files for auto re-build, added loop for system service behaviour

--- main.go
+++ main.go
@@ -1,20 +1,55 @@
 package main
 
 import (
-    "fmt"
-    "github.com/go-redis/redis/v8"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+	//"syscall"
 )
 
 func main() {
 
-	fmt.Println("Hello World")
-	
-	// client := redis.NewClient(&redis.Options{
-    //     Addr:     "edgebox-queue:6379",
-    //     Password: "", // no password set
-    //     DB:       0,  // use default DB
-    // })
-    // pong, err := client.Ping().Result()
-	// fmt.Println(pong, err)
-	
+	// load command line arguments
+
+	name := flag.String("name", "edgebox", "name for the service")
+
+	flag.Parse()
+
+	log.Printf("Starting Sysctl service for %s", *name)
+
+	// setup signal catching
+	sigs := make(chan os.Signal, 1)
+
+	// catch all signals since not explicitly listing
+	signal.Notify(sigs)
+
+	// Cathing specific signals can be done with:
+	//signal.Notify(sigs,syscall.SIGQUIT)
+
+	// method invoked upon seeing signal
+	go func() {
+		s := <-sigs
+		log.Printf("RECEIVED SIGNAL: %s", s)
+		AppCleanup()
+		os.Exit(1)
+	}()
+
+	// infinite loop
+	for {
+
+		log.Printf("Executing instruction %s", *name)
+
+		// wait random number of milliseconds
+		Nsecs := 1000
+		log.Printf("Next instruction executed in %dms", Nsecs)
+		time.Sleep(time.Millisecond * time.Duration(Nsecs))
+	}
+
+}
+
+// AppCleanup : cleanup app state before exit
+func AppCleanup() {
+	log.Println("Cleaning up app status before exit")
 }
