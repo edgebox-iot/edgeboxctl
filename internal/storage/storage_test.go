6d3c1df1f539bfa47842a3339ca98fa1e2e7dba8
Paulo Truta <pinheirotruta5@gmail.com>
2021-06-12 14:55:29 +0000

    TestGetDevices

--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -3,12 +3,29 @@
 package storage
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestGetDevices(t *testing.T) {
 	result := GetDevices()
 
-	fmt.Println(result)
+	if len(result) == 0 {
+		t.Log("Expecting at least 1 block device, 0 elements found in slice")
+		t.Fail()
+	}
+
+	foundDevice := false
+
+	t.Log("Looking for a mmcblk0 or sda device")
+	for _, device := range result {
+		if device.ID == "mmcblk0" || device.ID == "sda" {
+			t.Log("Found target device", device.ID)
+			foundDevice = true
+		}
+	}
+
+	if !foundDevice {
+		t.Log("Expected to find device mmcblk0 but did not. Devices:", result)
+		t.Fail()
+	}
 }
