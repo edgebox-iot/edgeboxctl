33ab2377c0eaff8dc550a8d04537e96e0e885cf3
Paulo Truta <pinheirotruta5@gmail.com>
2021-06-15 11:05:02 +0000

    Added missing import

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/edgebox-iot/edgeboxctl/internal/diagnostics"
 	"github.com/edgebox-iot/edgeboxctl/internal/utils"
 	"github.com/shirou/gopsutil/disk"
 )
