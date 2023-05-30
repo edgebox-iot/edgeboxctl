df13bf705d3182c130804da2c589ed5805ac06fe
Paulo Truta <pinheirotruta5@gmail.com>
2023-05-30 21:22:31 +0200

    Removed formatting directive from Println call

--- internal/tasks/tasks.go
+++ internal/tasks/tasks.go
@@ -150,7 +150,7 @@ func ExecuteTask(task Task) Task {
 			var args taskSetupBackupsArgs
 			err := json.Unmarshal([]byte(task.Args.String), &args)
 			if err != nil {
-				log.Println("Error reading arguments of setup_backups task: %s", err)
+				log.Println("Error reading arguments of setup_backups task: " + err)
 			} else {
 				taskResult := taskSetupBackups(args)
 				taskResultBool := true
